3f79d7798e8e3b410efcada8ff92337e26eff92e
grace njeri wambui <gracenjeriwambui00@gmail.com>
2023-05-30 16:17:43 +0300

    Bot Collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function BotCollection() {
+  const [bots, setBots] = useState([]);
   // Your code here
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+    .then(response => response.json())
+    .then(data => setBots(data))
+    .catch(error => console.error(error));
+  }, []);
+  
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {bots.map(bot => (
+          <div key={bot.id} className="column">
+            <img src={bot.avatar_url} alt={bot.name} />
+            <h3>{bot.name}</h3>
+            <p>Health: {bot.health}</p>
+            <p>Damage: {bot.damage}</p>
+            <p>Armor: {bot.armor}</p>
+            <p>Class: {bot.bot_class}</p>
+            <p>Catchphrase: {bot.catchphrase}</p>
+          </div>
+        ))}
         Collection of all bots
       </div>
     </div>
